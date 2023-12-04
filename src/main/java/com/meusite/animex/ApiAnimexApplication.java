ea48769a403ab68b92fc6ee8c3d94dc914dbdab8
Victor Gabriel <victorgabdev@gmail.com>
2023-12-04 16:44:25 -0300

    first commit

---
+++ src/main/java/com/meusite/animex/ApiAnimexApplication.java
@@ -0,0 +1,13 @@
+package com.meusite.animex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnimexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnimexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meusite/animex/ApiAnimexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meusite.animex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnimexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
