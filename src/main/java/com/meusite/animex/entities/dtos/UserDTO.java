a3458d05d3a3a8a3bc990efedd9b34d826bb5cb6
Victor Gabriel <victorgabdev@gmail.com>
2023-12-05 20:03:21 -0300

    Criar UserDTO

---
+++ src/main/java/com/meusite/animex/entities/dtos/UserDTO.java
@@ -0,0 +1,42 @@
+package com.meusite.animex.entities.dtos;
+
+import com.meusite.animex.entities.User;
+
+import java.util.UUID;
+
+public class UserDTO {
+
+    private UUID id;
+    private String name;
+    private String email;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
