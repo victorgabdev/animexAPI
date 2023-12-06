7b91ab8e106d8c0fe375fbfd4ef2770ee2b37bc7
Victor Gabriel <victorgabdev@gmail.com>
2023-12-06 18:59:40 -0300

    atualizar entity user

--- src/main/java/com/meusite/animex/entities/User.java
+++ src/main/java/com/meusite/animex/entities/User.java
@@ -85,14 +85,4 @@ public class User {
     public int hashCode() {
         return Objects.hash(getEmail());
     }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
